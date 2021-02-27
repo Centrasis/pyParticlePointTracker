06b24b2d4082b1f6b1225d17a885db0009977ff2
Felix Lehner <f.lehner@tu-bs.de>
2021-02-27 18:38:00 +0100

    adding test

--- tests/simple_test.py
+++ tests/simple_test.py
@@ -17,17 +17,33 @@ def test_integrity():
     ppt = ParticlePointTracker.ParticlePointTracker(gen_features, 40, 50, True)
     frame = np.zeros((500, 500), dtype=np.uint8)
     frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
-    frame = cv2.circle(frame, (50, 50), 10, (255, 50, 20), 2)
     points = np.array([[50, 50]], dtype=np.int)
     ppt.set_initial_points(points)
     ppt.step(frame)
 
     assert(ppt.resulting_points[0][0] == points[0][0] and ppt.resulting_points[0][1] == points[0][1])
 
+def test_basic_tracking():
+    ppt = ParticlePointTracker.ParticlePointTracker(gen_features, 40, 50, True)
+    frame = np.zeros((500, 500), dtype=np.uint8)
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+    frame = cv2.circle(frame, (50, 50), 10, (255, 50, 20), 2)
+    points = np.array([[50, 50]], dtype=np.int)
+    ppt.set_initial_points(points)
+    ppt.step(frame)
+
     frame = np.zeros((500, 500), dtype=np.uint8)
     frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
     frame = cv2.circle(frame, (50, 60), 10, (255, 50, 20), 2)
     ppt.step(frame)
 
-    assert(ppt.resulting_points[0][1] > 50 and ppt.resulting_points[0][1] < 70)
-    assert(ppt.resulting_points[0][0] > 45 and ppt.resulting_points[0][0] < 55)
+    assert(ppt.resulting_points[0][1] > 45 and ppt.resulting_points[0][1] < 75)
+    assert(ppt.resulting_points[0][0] > 40 and ppt.resulting_points[0][0] < 60)
+
+    frame = np.zeros((500, 500), dtype=np.uint8)
+    frame = cv2.cvtColor(frame, cv2.COLOR_GRAY2BGR)
+    frame = cv2.circle(frame, (50, 70), 10, (255, 50, 20), 2)
+    ppt.step(frame)
+
+    assert(ppt.resulting_points[0][1] > 55 and ppt.resulting_points[0][1] < 85)
+    assert(ppt.resulting_points[0][0] > 40 and ppt.resulting_points[0][0] < 60)
